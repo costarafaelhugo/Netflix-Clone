d9fd37bebd012863767803d4f6926cc3b9a338ca
Hugo Costa <costarafaelhugo@gmail.com>
2021-05-07 07:36:29 -0300

    Terminei a tela de cadastro

--- app/src/main/java/com/hugorafaelcosta/netflixclone/FormCadastro.kt
+++ app/src/main/java/com/hugorafaelcosta/netflixclone/FormCadastro.kt
@@ -1,7 +1,9 @@
 package com.hugorafaelcosta.netflixclone
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.hugorafaelcosta.netflixclone.databinding.ActivityFormCadastroBinding
 
 class FormCadastro : AppCompatActivity() {
@@ -13,11 +15,47 @@ class FormCadastro : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar!!.hide()
+        Toolbar()
 
-        val toolbar = binding.toolbarCadastro
-        toolbar.setBackgroundColor(getColor(R.color.white))
-        toolbar.setNavigationIcon(getDrawable(R.drawable.ic_netflix_official_logo))
+        binding.btCadastrar.setOnClickListener {
+            val email = binding.editEmail.text.toString()
+            val senha = binding.editSenha.text.toString()
+            val mensagem_erro = binding.mensagemErro
+
+            if (email.isEmpty() || senha.isEmpty()) {
+                mensagem_erro.setText("Preencha todos os campos!")
+            } else {
+                CadastrarUsuario()
+            }
+        }
 
 
+    }
+
+    private fun CadastrarUsuario() {
+        val email = binding.editEmail.text.toString()
+        val senha = binding.editSenha.text.toString()
+        val mensagem_erro = binding.mensagemErro
+        
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,senha).addOnCompleteListener{
+            if(it.isSuccessful){
+                Toast.makeText(this, "Usuário cadastrado com sucesso", Toast.LENGTH_SHORT).show()
+                binding.run {
+                    editEmail.run { setText("") }
+                    editSenha.run { setText("") }
+                    mensagem_erro.text = ""
+                }
+            }
+        }.addOnFailureListener{
+            mensagem_erro.text = "Erro ao cadastrar usuário"
+        }
+
+    }
+
+    private fun Toolbar() {
+        val toolbar = binding.toolbarCadastro.also {
+            it.setBackgroundColor(getColor(R.color.white))
+        }
+        toolbar.navigationIcon = getDrawable(R.drawable.ic_netflix_official_logo)
     }
 }
