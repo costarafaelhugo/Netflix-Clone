74a9fa41498ca6d7f198d47a9e8c3698b099239a
Hugo Costa <costarafaelhugo@gmail.com>
2021-05-10 12:54:09 -0300

    Configurei a biblioteca Picasso pra carregar a imagem do filme

--- app/src/main/java/com/hugorafaelcosta/netflixclone/DetalhesFilme.kt
+++ app/src/main/java/com/hugorafaelcosta/netflixclone/DetalhesFilme.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.GridLayoutManager
 import com.hugorafaelcosta.netflixclone.Adapter.FilmesAdapter
 import com.hugorafaelcosta.netflixclone.Model.addFilmes
 import com.hugorafaelcosta.netflixclone.databinding.ActivityDetalhesFilmeBinding
+import com.squareup.picasso.Picasso
 
 class DetalhesFilme : AppCompatActivity() {
     private lateinit var binding: ActivityDetalhesFilmeBinding
@@ -22,6 +23,8 @@ class DetalhesFilme : AppCompatActivity() {
         recycler_outros_filmes.adapter = FilmesAdapter(addFilmes())
         recycler_outros_filmes.layoutManager = GridLayoutManager(applicationContext, 3)
 
+        val capaTheWitcher = "https://firebasestorage.googleapis.com/v0/b/netflix-clone-76531.appspot.com/o/video.jpg?alt=media&token=45680ce9-ad2b-4cc8-a9a0-85212b122be0"
+        Picasso.get().load(capaTheWitcher).fit().into(binding.capa)
     }
 
     private fun Toolbar() {
