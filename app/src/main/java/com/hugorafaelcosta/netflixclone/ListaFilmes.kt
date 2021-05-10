6628cfafef037d586b52f8753636e59f1bb97a57
Hugo Costa <costarafaelhugo@gmail.com>
2021-05-10 08:42:50 -0300

    Configurei o evento de clique que abre a tela de detalhes do filme

--- app/src/main/java/com/hugorafaelcosta/netflixclone/ListaFilmes.kt
+++ app/src/main/java/com/hugorafaelcosta/netflixclone/ListaFilmes.kt
@@ -10,6 +10,8 @@ import androidx.recyclerview.widget.GridLayoutManager
 import com.google.firebase.auth.FirebaseAuth
 import com.hugorafaelcosta.netflixclone.Adapter.FilmesAdapter
 import com.hugorafaelcosta.netflixclone.Model.addFilmes
+import com.hugorafaelcosta.netflixclone.OnClick.OnItemClickListener
+import com.hugorafaelcosta.netflixclone.OnClick.addOnItemClickListener
 import com.hugorafaelcosta.netflixclone.databinding.ActivityListaFilmesBinding
 
 class ListaFilmes : AppCompatActivity() {
@@ -23,6 +25,20 @@ class ListaFilmes : AppCompatActivity() {
         val recycler_filmes = binding.recyclerview
         recycler_filmes.adapter = FilmesAdapter(addFilmes())
         recycler_filmes.layoutManager = GridLayoutManager(applicationContext, 3)
+
+        recycler_filmes.addOnItemClickListener(object:OnItemClickListener{
+            override fun onItemClicked(position: Int, view: View) {
+              when{
+                  position == 0 -> DetalhesFilme()
+              }
+            }
+        })
+
+    }
+
+    private fun DetalhesFilme(){
+        val intent = Intent(this, DetalhesFilme::class.java)
+        startActivity(intent)
 
     }
 
